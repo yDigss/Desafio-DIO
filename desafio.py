ba5ff128f69c8f5e2880baa50e9f29a215e81196
Rodrigo Silva <rodrigo.7762018@gmail.com>
2025-03-16 11:36:35 -0300

    funcao filtrar ususuario e criar usario adicionadas

--- desafio.py
+++ desafio.py
@@ -50,3 +50,32 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
         print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
     return saldo, extrato
+
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print("=========================================")
+
+
+def criar_usuario(usuarios):
+    cpf = int(input("Informe o CPF (somente números): "))
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+    
+    nome = str(input("Informe o nome completo: "))
+    data_nascimento = int(input("Informe a data de nascimento (dd-mm-aaaa): "))
+    endereco = str(input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): "))
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com suscesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
